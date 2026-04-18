7b604f598d1670b2d10680c557166de3f12a8cbd
DevJuloAbe <devjulo2026@gmail.com>
2026-04-18 13:35:27 +0800

    Added a global CORS header middleware

--- src/app.js
+++ src/app.js
@@ -12,6 +12,19 @@ import { authenticateAdminJwt } from "./middleware/authenticateAdminJwt.js";
 export function createApp() {
   const app = express();
 
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    // res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(204);
+    }
+
+    return next();
+  });
+
   const allowedOrigins =
     env.corsOrigins.length > 0
       ? env.corsOrigins
