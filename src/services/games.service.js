f8d7e1d73e80cd14d8577166b2afefa704cb801e
DevJuloAbe <devjulo2026@gmail.com>
2026-04-27 10:47:53 +0800

    includeGameUrl

--- src/services/games.service.js
+++ src/services/games.service.js
@@ -6,6 +6,20 @@ import {
   listPublicGames,
 } from "../repositories/games.repository.js";
 
+function includeGameUrl(game) {
+  if (!game) {
+    return game;
+  }
+
+  const { id, game_id, game_url, ...rest } = game;
+  return {
+    id,
+    game_id: game_id ?? null,
+    game_url: game_url ?? null,
+    ...rest,
+  };
+}
+
 export async function getGamesCatalog(options = {}) {
   const featuredLimit = options.featuredLimit ?? 10;
   const newLimit = options.newLimit ?? 10;
@@ -19,19 +33,19 @@ export async function getGamesCatalog(options = {}) {
   return {
     success: true,
     data: {
-      games,
-      featured_games: featuredGames,
-      new_games: newGames,
+      games: games.map(includeGameUrl),
+      featured_games: featuredGames.map(includeGameUrl),
+      new_games: newGames.map(includeGameUrl),
     },
   };
 }
 
 export async function getGameDetailsById(gameId) {
-  return findGameById(gameId, { includeGameSecretKey: true });
+  return includeGameUrl(await findGameById(gameId, { includeGameSecretKey: true }));
 }
 
 export async function getGameDetailsBySlug(slug) {
-  return findGameBySlug(slug, { includeGameSecretKey: true });
+  return includeGameUrl(await findGameBySlug(slug, { includeGameSecretKey: true }));
 }
 
 export async function getGameDetailsByIdentifier(identifier) {
