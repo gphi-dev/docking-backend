3bb031c512b36844a0723f0b13aaaf18a5b34f5e
DevJuloAbe <devjulo2026@gmail.com>
2026-04-27 17:27:07 +0800

    asdasdbonak env

--- src/config/env.js
+++ src/config/env.js
@@ -52,8 +52,8 @@ export const env = {
   seedAdminUsername: process.env.SEED_ADMIN_USERNAME || "admin",
   seedAdminPassword: process.env.SEED_ADMIN_PASSWORD || "changeme",
   aws: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "AKIAXP5FYDKS6HASL3GF",
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "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",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
     region: process.env.AWS_REGION || "ap-southeast-1",
     s3Bucket: process.env.AWS_S3_BUCKET || "gphi-docking-public",
     s3PublicUrl: normalizeUrl(process.env.AWS_S3_PUBLIC_URL) || "https://gphi-docking-public.s3.ap-southeast-1.amazonaws.com",
